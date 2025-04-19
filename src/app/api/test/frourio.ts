df15b832f31dbf4adaed19941c7448e08836fbeb
riku <opa9788@gmail.com>
2025-04-19 12:52:39 +0900

    frourio nextを導入

---
+++ src/app/api/test/frourio.ts
@@ -0,0 +1,24 @@
+import type { FrourioSpec } from "@frourio/next";
+import { headers } from "next/headers";
+import { z } from "zod";
+
+export const frourioSpec = {
+  get: {
+    res: {
+      200: {
+        body: z.object({ value: z.string() }),
+      },
+    },
+  },
+  post: {
+    body: z.object({ id: z.string() }),
+    res: {
+      200: {
+        body: z.object({ value: z.string() }),
+      },
+      404: {
+        body: z.object({ error: z.string() }),
+      },
+    },
+  },
+} satisfies FrourioSpec;
