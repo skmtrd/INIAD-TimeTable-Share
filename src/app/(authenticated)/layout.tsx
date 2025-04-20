63b43f7d68448e897756526bffa83ff75e8aa22a
skmtrd <skmtgame23@gmail.com>
2025-04-20 14:20:18 +0900

    biome 設定更新

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,55 +1,14 @@
 import { SiteHeader } from "@/components/common/SiteHeader";
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-// MUIのテーマを設定
-// const theme = createTheme({
-//   palette: {
-//     mode: "light",
-//     primary: {
-//       main: "#1976d2",
-//     },
-//     secondary: {
-//       main: "#9c27b0",
-//     },
-//   },
-//   typography: {
-//     fontFamily: [
-//       "-apple-system",
-//       "BlinkMacSystemFont",
-//       '"Segoe UI"',
-//       "Roboto",
-//       '"Helvetica Neue"',
-//       "Arial",
-//       "sans-serif",
-//       '"Apple Color Emoji"',
-//       '"Segoe UI Emoji"',
-//       '"Segoe UI Symbol"',
-//     ].join(","),
-//   },
-//   components: {
-//     MuiAppBar: {
-//       defaultProps: {
-//         color: "default",
-//       },
-//       styleOverrides: {
-//         root: {
-//           backgroundColor: "rgba(255, 255, 255, 0.8)",
-//         },
-//       },
-//     },
-//   },
-// });
+import type React from "react";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 const Layout: React.FC<LayoutProps> = (props) => (
-  // <ThemeProvider theme={theme}>
   <main className="flex min-h-screen w-full flex-col">
     <SiteHeader />
     <div className="flex-1">{props.children}</div>
   </main>
-  // </ThemeProvider>
 );
 
 export default Layout;
--- src/components/common/PageContainer.tsx
+++ src/components/common/PageContainer.tsx
@@ -1,5 +1,5 @@
 import { Container } from "@mui/material";
-
+import type React from "react";
 interface PageContainerProps {
   /**
    * ページのタイトル
