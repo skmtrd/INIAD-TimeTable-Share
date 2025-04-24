01b3c3e3b513e4d4101a9b33a1613e263b610d85
riku <opa9788@gmail.com>
2025-04-24 20:49:17 +0900

    時間割登録api完成

---
+++ src/app/api/timetable/register/route.ts
@@ -0,0 +1,124 @@
+import { PrismaClient } from "@prisma/client";
+
+import { createRoute } from "./frourio.server";
+
+type Timetable = {
+  [periodNumber: string]: string[];
+};
+
+export const { POST } = createRoute({
+  post: async ({ body }) => {
+    let timetableObject: Timetable = {};
+
+    try {
+      timetableObject = await convertJsonFileToObject(body.file);
+    } catch {
+      return { status: 400, body: { message: "Invalid file" } };
+    }
+
+    try {
+      handleRegister(timetableObject, body.userId);
+    } catch {
+      return { status: 500, body: { message: "Internal Server Error" } };
+    }
+
+    return { status: 201, body: { message: "register timetable success" } };
+  },
+});
+
+const convertJsonFileToObject = async (jsonfile: File): Promise<Timetable> => {
+  const text = await jsonfile.text();
+  const timetableObject: Timetable = JSON.parse(text);
+
+  return timetableObject;
+};
+
+const days: Record<number, string> = {
+  0: "月曜日",
+  1: "火曜日",
+  2: "水曜日",
+  3: "木曜日",
+  4: "金曜日",
+  5: "土曜日",
+};
+
+const handleRegister = (timetable: Timetable, userId: string) => {
+  Object.entries(timetable).map(
+    ([periodNumber, lectures]: [string, string[]]) => {
+      lectures.map((lecture: string, dayIndex: number) => {
+        if (lecture === "") return;
+
+        registerFacade(lecture, Number(periodNumber), days[dayIndex], userId);
+      });
+    },
+  );
+};
+
+const registerFacade = async (
+  lectureName: string,
+  periodNumber: number,
+  day: string,
+  userId: string,
+) => {
+  if (await isExitsLecture(lectureName)) {
+    const lectureId: string = await getLectureId(lectureName);
+    registerLectureToTimetable(userId, lectureId);
+  } else {
+    const lecture = await addNewLecture(lectureName, periodNumber, day);
+    registerLectureToTimetable(userId, lecture.id);
+  }
+};
+
+const getLectureId = async (lectureName: string): Promise<string> => {
+  const prisma = new PrismaClient();
+  const lecture = await prisma.lecture.findFirst({
+    where: { name: lectureName },
+  });
+
+  if (!lecture) {
+    throw new Error("Lecture not found");
+  }
+
+  return lecture.id;
+};
+
+const isExitsLecture = async (lectureName: string): Promise<boolean> => {
+  const prisma = new PrismaClient();
+  const lecture = await prisma.lecture.findFirst({
+    where: { name: lectureName },
+  });
+
+  const isExit = lecture !== null;
+
+  return isExit;
+};
+
+const addNewLecture = async (
+  lectureName: string,
+  periodNumber: number,
+  day: string,
+) => {
+  const prisma = new PrismaClient();
+  const lecture = await prisma.lecture.create({
+    data: {
+      name: lectureName,
+      day: day,
+      periodNumber: periodNumber,
+    },
+  });
+
+  return lecture;
+};
+
+const registerLectureToTimetable = async (
+  userId: string,
+  lectureId: string,
+): Promise<void> => {
+  const prisma = new PrismaClient();
+  await prisma.timeTable.create({
+    data: {
+      userId: userId,
+      lectureId: lectureId,
+    },
+  });
+};
