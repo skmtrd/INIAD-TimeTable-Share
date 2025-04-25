18bab9ce72afc0feaf85d50d904477c2f3a30d1f
riku <opa9788@gmail.com>
2025-04-26 05:33:52 +0900

    特定の授業を履修しているユーザーの一覧を取得するapiを作成

---
+++ src/app/api/lecture/[lectureId]/participant/route.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import { createRoute } from "./frourio.server";
+
+export const { GET } = createRoute({
+  get: async (req) => {
+    const lectureId: string = req.params.lectureId;
+
+    try {
+      const prisma = new PrismaClient();
+      const participants = await prisma.timeTable.findMany({
+        where: {
+          lectureId: lectureId,
+        },
+        select: {
+          lectureId: true,
+          user: {
+            select: {
+              name: true,
+              id: true,
+              image: true,
+              twitterId: true,
+            },
+          },
+        },
+      });
+
+      return { status: 200, body: participants };
+    } catch {
+      return { status: 500, body: { message: "internal server error" } };
+    }
+  },
+});
