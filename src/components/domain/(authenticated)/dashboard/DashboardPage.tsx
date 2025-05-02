507c52ef06614d277f3be13446d897f8f794d1cc
skmtrd <skmtgame23@gmail.com>
2025-05-02 17:54:05 +0900

    ダッシュボードページに外部リンクと説明文を追加し、アップロードフォームのレイアウトを改善しました。

--- src/components/domain/(authenticated)/dashboard/DashboardPage.tsx
+++ src/components/domain/(authenticated)/dashboard/DashboardPage.tsx
@@ -3,7 +3,7 @@
 import PageContainer from "@/components/common/PageContainer";
 import UploadForm from "@/components/domain/(authenticated)/dashboard/UploadForm";
 import { useDashboard } from "@/hooks/domain/(authenticated)/dashboard/useDashboard";
-import { Box, CircularProgress } from "@mui/material";
+import { Box, CircularProgress, Link, Typography } from "@mui/material";
 import Timetable from "../profile/Timetable";
 
 const DashboardPage = () => {
@@ -31,7 +31,30 @@ const DashboardPage = () => {
             isAccessUserPage={true}
           />
         ) : (
-          <UploadForm timetableMutate={timetableMutate} />
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: 6,
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: "flex" }}>
+              <Link
+                href="https://www.ace.toyo.ac.jp/ct/home"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="h6"
+              >
+                Toyo-Net Ace
+              </Link>
+              <Typography variant="h6">
+                にアクセスして時間割のjsonファイルをダウンロード
+              </Typography>
+            </Box>
+            <UploadForm timetableMutate={timetableMutate} />
+          </Box>
         )}
       </Box>
     </PageContainer>
